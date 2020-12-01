62654c6f6db7a967e035a68959300d3e26af1e10
Silas K <silk6174@gmail.com>
2020-12-01 15:06:51 +0100

    Reset advent_19/advent_2_1.c and advent_19/advent_2_2.c to blank slates, was code from advent_19/advent_1_1.c

--- advent_19/2/advent_2_2.c
+++ advent_19/2/advent_2_2.c
@@ -1,32 +1,7 @@
 
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 
-#define MAX_DIGITS 10
-
-double mass_to_fuel(double mass)
+int main() 
 {
-	double fuel = (floor(mass / 3) - 2);
-	
-	if (fuel <= 0) {
-		return 0;
-	} else {
-		return fuel + mass_to_fuel(fuel);
-	}
-}
 
-int main()
-{
-	double fuel_sum = 0;
-
-	/* Read in numbers */
-	char number_str[MAX_DIGITS];
-	while (fgets(number_str, MAX_DIGITS, stdin))
-		fuel_sum += mass_to_fuel( atof(number_str) );
-	
-
-	printf("%lf\n", fuel_sum);
-
-	return (0);
 }
